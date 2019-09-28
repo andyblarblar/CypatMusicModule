aa889b3e6e0cb18a4fd81d8c9d7c8d624363e9f4
Andy Ealovega <andyblarblar@gmail.com>
2019-09-27 22:20:05 -0400

    Added Linux support.

--- CypatMusicModule/Program.cs
+++ CypatMusicModule/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Drawing;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using Console = Colorful.Console;
 
 namespace CypatMusicModule
@@ -98,8 +100,104 @@ namespace CypatMusicModule
             var isLinux = Environment.OSVersion.Platform == PlatformID.Unix;
 
             if (isLinux)
-            {
-                /* Linux track */
+            {   /*
+                if (!Directory.Exists(@"/opt/CyberPatriot"))
+                {
+                    Console.WriteLine("You silly goose! This isnt a cypat VM!\n");
+                    return;
+                }
+                */
+                Console.WriteLine("What do you want to replace the score gain sound with?\n" +
+                                  "1) This is a good result!\n" +
+                                  "2) noice\n" +
+                                  "3) MC level up\n" +
+                                  "C) custom\n" +
+                                  "Literally anything else) dont change");
+                var response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "1":
+                        WriteSoundFile(true, isLinux, "good-result.wav");
+                        break;
+
+                    case "2":
+                        WriteSoundFile(true, isLinux, "nice.wav");
+                        break;
+
+                    case "3":
+                        WriteSoundFile(true, isLinux, "levelup.wav");
+                        break;
+
+                    case "C":
+                        Console.WriteLine("please enter the name of your file...");
+                        var input = Console.ReadLine();
+                        try
+                        {
+                            WriteSoundFile(true, isLinux, input);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Failed to write file", Color.Crimson);
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("alrighty");
+                        break;
+                }
+
+                Console.WriteLine("And what about the score lose and siren sound?\n" +
+                                  "1) OOF!\n" +
+                                  "2) scott the woz (oh heck a swear)\n" +
+                                  "3) 56k\n" +
+                                  "4) nope\n" +
+                                  "5) seinfeld\n" +
+                                  "C) custom" +
+                                  "anything else) dont change");
+
+                response = Console.ReadLine();
+
+                switch (response)
+                {
+                    case "1":
+                        WriteSoundFile(false, isLinux, "roblox-death-sound_1.wav");
+                        break;
+
+                    case "2":
+                        WriteSoundFile(false, isLinux, "scottDARNIT.wav");
+                        break;
+
+                    case "3":
+                        WriteSoundFile(false, isLinux, "56k.wav");
+                        break;
+
+                    case "4":
+                        WriteSoundFile(false, isLinux, "nope.wav");
+                        break;
+
+                    case "5":
+                        WriteSoundFile(false, isLinux, "seinfeld.wav");
+                        break;
+
+                    case "C":
+                        Console.WriteLine("please enter the name of your file...");
+                        var input = Console.ReadLine();
+                        try
+                        {
+                            WriteSoundFile(false, isLinux, input);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Failed to write file", Color.Crimson);
+                        }
+
+                        break;
+
+                }
+
+                return;
 
             }
 
@@ -204,16 +302,30 @@ namespace CypatMusicModule
             }
 
             
-            static void WriteSoundFile(bool isGain, bool isLinux, string fileName)//TODO linux
+            static void WriteSoundFile(bool isGain, bool isLinux, string fileName)
             {
                 if (isLinux)
                 {
-                    //linux stuff
+                    var file2 = File.ReadAllBytes("musicFiles/" + fileName); 
+                    
 
+                    if (isGain)
+                    {
+                        File.WriteAllBytes(@"/opt/CyberPatriot/gain.wav", file2);
+                        return;
+                    }
+
+                    else
+                    {
+                        File.WriteAllBytes(@"/opt/CyberPatriot/alarm.wav", file2);
+                        File.WriteAllBytes(@"/opt/CyberPatriot/siren.wav", file2);
+                    }
+
+                    return;
                 }
 
                 var file = File.ReadAllBytes("musicFiles\\" + fileName); //windows stuff
-
+               
                 if (isGain)
                 {
                     File.WriteAllBytes(@"C:\CyberPatriot\gain.wav", file);
@@ -229,5 +341,28 @@ namespace CypatMusicModule
 
             }
         }
+
+
+
+        /// <summary>
+        /// Read contents of an embedded resource file as bytes
+        /// </summary>
+        /// <param name="filename">just the files name ex. file.txt</param>
+        public static byte[] ReadResourceFileAsBytes(string filename)
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+            using (var stream = thisAssembly.GetManifestResourceStream(thisAssembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(filename))))
+            {
+                if (stream == null) return null;
+                byte[] ba = new byte[stream.Length];
+                stream.Read(ba, 0, ba.Length);
+                return ba;
+
+            }
+        }
+
+
+
     }
 }
